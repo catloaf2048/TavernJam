47ed8c8c6548d135b9a013c9c6ef4c6307aa0614
voidwing <catloaf2048@proton.me>
2026-03-25 15:37:23 -0300

    Added simple foe.

---
+++ src/GDScript/SimpleFoe.gd
@@ -0,0 +1,32 @@
+class_name SimpleFoe;
+extends RigidBody2D;
+
+@export var wallCheck:Area2D;
+@export var hitbox:Area2D;
+@export var speed:float = 150;
+
+var direction:float;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	wallCheck.area_entered.connect(wallHit);
+	hitbox.area_entered.connect(hitboxHit);
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# this will be weird, and will not be deterministic but is jam. i care not.
+	linear_velocity.x = lerp(linear_velocity.x, direction*speed, delta);
+	linear_velocity.y += 10*delta;
+	pass;
+
+func wallHit(thing:Node2D) -> void:
+	direction *= -1;
+	pass;
+
+func hitboxHit(thing:Node2D) -> void:
+	if(thing.is_in_group('Player')):
+		# nice thing about python like languages, i can just call random bunk and it wont yell at me.
+		thing.Damage();
+	pass;
