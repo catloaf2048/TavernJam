046d8d2dc6d05439839d9000251b9869fde1a3eb
voidwing <catloaf2048@proton.me>
2026-03-27 14:18:57 -0300

    update foe gravity.

--- src/GDScript/SimpleFoe.gd
+++ src/GDScript/SimpleFoe.gd
@@ -23,7 +23,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	# this will be weird, and will not be deterministic but is jam. i care not.
 	linear_velocity.x = lerp(linear_velocity.x, direction*speed, delta);
-	linear_velocity.y += 10*delta;
+	linear_velocity.y += get_gravity().y*delta;
 	pass;
 
 func wallHit(thing:Node2D) -> void:
