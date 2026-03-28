20b3240d5e86c15a0c17c6bd703f053afb75f3b6
voidwing <catloaf2048@proton.me>
2026-03-28 09:30:46 -0300

    Added healthbar functionality.

--- src/GDScript/UIManager.gd
+++ src/GDScript/UIManager.gd
@@ -4,12 +4,15 @@ extends CanvasLayer
 @export var player:PlayerController;
 @export var deathCutscene:PackedScene;
 @export var uiMan:CanvasLayer
+@export var healthBar:TextureProgressBar
 
 var deathCutsceneNode:Node;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.playerDied.connect(playerDied);
+	player.playerHurt.connect(playerHit);
+	initHpBar();
 	pass; # Replace with function body.
 
 
@@ -21,7 +24,19 @@ func playerDied():
 	deathCutsceneNode = deathCutscene.instantiate();
 	
 	var anim = deathCutsceneNode.get_child(0) as AnimationPlayer;
-	var animDone = func(unused): deathCutsceneNode.queue_free();
+	var animDone = func(ignored): deathCutsceneNode.queue_free();
 	anim.animation_finished.connect(animDone);
 	uiMan.add_child(deathCutsceneNode);
+	initHpBar();
+	pass;
+
+func playerHit(ignored):
+	healthBar.value=player.hp;
+	var anim = healthBar.get_child(0) as AnimationPlayer;
+	anim.play('default');
+	pass;
+
+func initHpBar():
+	healthBar.max_value = player.maxHp;
+	healthBar.value = player.hp;
 	pass;
